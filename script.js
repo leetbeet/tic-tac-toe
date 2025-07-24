1a01a6a37e62f15f0e92c4acf8d755a9bbfeea68
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-07-24 09:16:02 +0800

    Add variables in function to play the game

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ const createPlayer = (name, markType) => {
         getMarkType() {
             return markType;
         }
-    }
-}
+    };
+};
+
+const playGame = (() => {
+    const gameArr = Array(9).fill("");
+    const player1 = createPlayer("A", "X");
+    const player2 = createPlayer("B", "O");
+
+    const ThreeInARow = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]             
+    ];
+    
+})();
